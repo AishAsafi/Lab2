2c4425c04f01daf35f582c01f17581686cde7842
Aisha <marena665@hotmail.com>
2019-12-06 00:32:49 +0100

    lab2

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int addition (int x, int y){
+        return x+y;
+    }
+
+    public int subtraktion (int x, int y){
+        return x-y;
+    }
+
+    int multiplikation(int x, int y){
+        return x*y;
+    }
+
+    public double division (int x, int y){
+
+        return x/y;
+    }
+}
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.*;
+//@FixMethodOrder(MethodSorters.NAME_ASCENDING);
+
+class CalculatorTest {
+
+    Calculator calculator;
+
+
+    @BeforeEach
+    public void beforeMethod(){
+
+        calculator=new Calculator();
+    }
+
+
+    @Test
+    @DisplayName("Testing add method ")
+    void testAddition() {
+        Assertions.assertEquals(40,calculator.addition(15,25));
+    }
+
+    @Test
+    @DisplayName("Testing subtraction method ")
+    void testSubtraktion() {
+        Assertions.assertEquals(10,calculator.subtraktion(25,15));
+    }
+
+    @Test
+    void testMultiplikation() {
+        Assertions.assertEquals(50,calculator.multiplikation(5,10));
+    }
+
+    @Test
+
+    void testDivision( ) {
+        Assertions.assertThrows(ArithmeticException.class, ()-> calculator.division(50,0),"Error: divid by Zero" );
+
+
+    }
+
+
+    @AfterEach
+    public void afterMethod(){
+
+        System.out.println("Test is finished. ");
+    }
+
+
+
+}
